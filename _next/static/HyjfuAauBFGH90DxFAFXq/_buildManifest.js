139169ead94ace4e2c4a5062dfdb5844516ac00c
Galyna Kud <galynakud121@gmail.com>
2026-02-27 20:53:04 +0200

    Updates

--- _next/static/HyjfuAauBFGH90DxFAFXq/_buildManifest.js
+++ _next/static/HyjfuAauBFGH90DxFAFXq/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/clever-todo-list//_next/:path+",
-        "destination": "/clever-todo-list/_next/:path+"
+        "source": "/mindlist-cleargoals//_next/:path+",
+        "destination": "/mindlist-cleargoals/_next/:path+"
       }
     ],
     "fallback": []
